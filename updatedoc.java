fbd5d88b785784a6543944d02d862e2b1fbd2548
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-04-30 10:12:36 +0530

    Add files via upload

---
+++ updatedoc.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.docDbHandle;
+import model.doctor;
+
+
+/**
+ *
+ * @author Dulani Anupama
+ */
+public class updatedoc extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        PrintWriter out=response.getWriter();  
+          
+        String sid=request.getParameter("id");  
+        int id=Integer.parseInt(sid);  
+        String name=request.getParameter("name");  
+          
+        String email=request.getParameter("email"); 
+        String number=request.getParameter("number");
+        String specialization=request.getParameter("specialization");
+        String city=request.getParameter("city");
+        String address=request.getParameter("address");
+          
+        doctor d=new doctor();  
+        d.setId(id);  
+        d.setName(name);    
+        d.setEmail(email);  
+        d.setNumber(number);
+        d.setSpecialization(specialization);  
+        d.setCity(city);  
+        d.setAddress(address);
+          
+        int status=docDbHandle.update(d);  
+        if(status>0){  
+            response.sendRedirect("ViewEmpServlet?page=1");  
+        }else{  
+            out.println("Sorry! unable to update record");  
+        }  
+          
+        out.close(); 
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ updatepa.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.patient;
+import model.patientDbHandle;
+
+/**
+ *
+ * @author Dulani Anupama
+ */
+public class updatepa extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        PrintWriter out=response.getWriter();  
+          
+        String sid=request.getParameter("id");  
+        int id=Integer.parseInt(sid);  
+        String name=request.getParameter("name");  
+          
+        String email=request.getParameter("email"); 
+        String number=request.getParameter("number");
+        String deseced=request.getParameter("deseced");
+        String bloodgroup=request.getParameter("bloodgroup");
+        String address=request.getParameter("address");
+          
+        patient p=new patient();  
+        p.setId(id);  
+        p.setName(name);    
+        p.setEmail(email);  
+        p.setNumber(number);
+        p.setDeseced(deseced);  
+        p.setBloodgroup(bloodgroup);  
+        p.setAddress(address);
+          
+        int status=patientDbHandle.update(p);  
+        if(status>0){  
+            response.sendRedirect("ViewEmpServlet?page=1");  
+        }else{  
+            out.println("Sorry! unable to update record");  
+        }  
+          
+        out.close(); 
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
